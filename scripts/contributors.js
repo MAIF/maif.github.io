e485c68f7183cafd34bca2e030a8cacf1c69c179
Etienne ANNE <etienne.anne@icloud.com>
2022-11-08 11:04:40 +0100

    Update contributors.js

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch');
 const fs = require('fs-extra');
 const _ = require('lodash');
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN || process.argv[2] || 'secret';
-const contributors_file = './social-data/contributors.json';
+const GITHUB_TOKEN = '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';
+const contributors_file = 'contributors.json';
 const filterOut = [
   'project-template',
   'otoroshi-jar-clevercloud-template',
@@ -37,9 +37,9 @@ function getCommits(repo) {
             commits_count: contributor.total,
           }
         })
-        .filter(c => c.name !== 'gitter-badger')
-        .filter(c => c.name !== 'snyk-bot')
-        .filter(c => c.name !== 'dependabot[bot]'), c => c.commits_count).reverse()
+          .filter(c => c.name !== 'gitter-badger')
+          .filter(c => c.name !== 'snyk-bot')
+          .filter(c => c.name !== 'dependabot[bot]'), c => c.commits_count).reverse()
       })
     } else {
       return [];
@@ -59,64 +59,77 @@ function getRepos() {
         authorization: `Bearer ${GITHUB_TOKEN}`
       }
     }).then(r => {
+      console.log(r)
       if (r.status === 200) {
-        return r.json().then(repositories => {
-          const results = {};
-          const tasks = [...repositories]
-            .filter(r => r.private === false)
-            .filter(r => r.fork === false)
-            .filter(r => filterOut.indexOf(r.name) === -1);
-  
-          function next() {
-            if (tasks.length === 0) {
-              success({
-                projects: results,
-                contributors: Object.keys(global).map(k => global[k]).map(_user => {
+        return r.json()
+          .then(repositories => {
+            const results = {};
+            const tasks = [...repositories]
+              .filter(r => r.private === false)
+              .filter(r => r.fork === false)
+              .filter(r => filterOut.indexOf(r.name) === -1);
+
+            function next() {
+              if (tasks.length === 0) {
+                console.log(results)
+                console.log(Object.keys(global).map(k => global[k]).map(_user => {
                   const user = { ..._user, projects: projectsPerUser[_user.name] };
                   return user
                 }).reduce((prev, curr) => {
                   prev[curr.name] = curr;
                   return prev;
-                }, {}),
-                commits_per_contributor_sorted: _.sortBy(Object.keys(global).map(k => global[k]), c => c.commits_count).reverse().map(i => i.name),
-              })
-            } else {
-              const task = tasks.pop();
-              getCommits(task.name).then(commits => {
-                results[task.name] = {
-                  name: task.name,
-                  url: task.html_url,
-                  description: task.description,
-                  homepage: task.homepage,
-                  pushed_at: task.pushed_at,
-                  contributors: commits.map(c => c.name),
-                  commits_count: commits.map(c => c.commits_count).reduce((a, b) => a + b, 0),
-                  commits_per_contributor: commits,
-                };
-                commits.map(commit => {
-                  const gcommit = global[commit.name];
-                  if (!gcommit) {
-                    global[commit.name] = { ...commit, commits_count: 0 };
-                  }
-                  global[commit.name].commits_count = global[commit.name].commits_count + commit.commits_count;
+                }, {}));
+                console.log(_.sortBy(Object.keys(global).map(k => global[k]), c => c.commits_count).reverse().map(i => i.name))
+                success({
+                  projects: results,
+                  contributors: Object.keys(global).map(k => global[k]).map(_user => {
+                    const user = { ..._user, projects: projectsPerUser[_user.name] };
+                    return user
+                  }).reduce((prev, curr) => {
+                    prev[curr.name] = curr;
+                    return prev;
+                  }, {}),
+                  commits_per_contributor_sorted: _.sortBy(Object.keys(global).map(k => global[k]), c => c.commits_count).reverse().map(i => i.name),
                 })
-                console.log(task.name);
-                next();
-              });
+              } else {
+                const task = tasks.pop();
+                getCommits(task.name).then(commits => {
+                  results[task.name] = {
+                    name: task.name,
+                    url: task.html_url,
+                    description: task.description,
+                    homepage: task.homepage,
+                    pushed_at: task.pushed_at,
+                    contributors: commits.map(c => c.name),
+                    commits_count: commits.map(c => c.commits_count).reduce((a, b) => a + b, 0),
+                    commits_per_contributor: commits,
+                  };
+                  commits.map(commit => {
+                    const gcommit = global[commit.name];
+                    if (!gcommit) {
+                      global[commit.name] = { ...commit, commits_count: 0 };
+                    }
+                    global[commit.name].commits_count = global[commit.name].commits_count + commit.commits_count;
+                  })
+                  console.log(task.name);
+                  next();
+                });
+              }
             }
-          }
-  
-          next();
-        })
+
+            next();
+          })
       } else {
         return [];
       }
     });
   });
 
-  
+
 }
 
-getRepos().then(results => {
-  fs.writeFileSync(contributors_file, JSON.stringify(results, null, 2));
-});
+getRepos()
+  .then(results => {
+    console.log(results)
+    fs.writeFileSync(contributors_file, JSON.stringify(results, null, 2));
+  });
