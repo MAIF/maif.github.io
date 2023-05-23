8e04577335f33195dc9c718e14f11f93e57bc6fb
baudelotphilippe <philippe@baudelot.eu>
2023-05-23 16:49:04 +0200

    oups change link

--- js/infos.js
+++ js/infos.js
@@ -2,17 +2,51 @@ const infosRepos = {
   otoroshi: {
     url: "https://github.com/MAIF/otoroshi",
     img: "otoroshi.svg",
+    url_project: "https://maif.github.io/otoroshi",
   },
-  eurybia: { url: "https://github.com/maif/eurybia", img: "eurybia.svg" },
-  thoth: { url: "https://github.com/MAIF/thoth", img: "thoth.svg" },
-  nio: { url: "https://github.com/maif/nio", img: "nio.svg" },
-  izanami: { url: "https://github.com/maif/izanami", img: "izanami.svg" },
-  daikoku: { url: "https://github.com/maif/daikoku", img: "daikoku.svg" },
-  cards: { url: "https://github.com/maif/cards", img: "cards.svg" },
+  eurybia: {
+    url: "https://github.com/maif/eurybia",
+    img: "eurybia.svg",
+    url_project: "https://maif.github.io/eurybia",
+  },
+  thoth: {
+    url: "https://github.com/MAIF/thoth",
+    img: "thoth.svg",
+    url_project: "https://maif.github.io/thoth",
+  },
+  nio: {
+    url: "https://github.com/maif/nio",
+    img: "nio.svg",
+    url_project: "https://maif.github.io/nio",
+  },
+  izanami: {
+    url: "https://github.com/maif/izanami",
+    img: "izanami.svg",
+    url_project: "https://maif.github.io/izanami",
+  },
+  daikoku: {
+    url: "https://github.com/maif/daikoku",
+    img: "daikoku.svg",
+    url_project: "https://maif.github.io/daikoku",
+  },
+  cards: {
+    url: "https://github.com/maif/cards",
+    img: "cards.svg",
+    url_project: "https://maif.github.io/cards",
+  },
   melusine: {
     url: "https://github.com/maif/melusine",
     img: "melusine.svg",
+    url_project: "https://maif.github.io/melusine",
   },
-  shapash: { url: "https://github.com/maif/shapash", img: "shapash.svg" },
-  "lets-automate": { url: "https://github.com/maif/lets-automate", img: "lets.svg" }
+  shapash: {
+    url: "https://github.com/maif/shapash",
+    img: "shapash.svg",
+    url_project: "https://maif.github.io/shapash",
+  },
+  "lets-automate": {
+    url: "https://github.com/maif/lets-automate",
+    img: "lets.svg",
+    url_project: "https://maif.github.io/nio",
+  },
 };
