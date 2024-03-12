da8b02f5ec48d366a77d8497d7e4cb5e915603de
Etienne ANNE <etienne.anne@icloud.com>
2024-03-12 09:59:10 +0100

    reduce interval

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -32,7 +32,7 @@ function getCommits(repo, attempts) {
         console.log(repo, 'retry')
         setTimeout(() => {
           resolve(getCommits(repo, attempts - 1))
-        }, 30000)
+        }, 10000)
       });
     }
     else if (r.status === 200) {
