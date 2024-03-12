43dd67cb9664ec155ccb6670f60bb238bebe57ed
Etienne ANNE <etienne.anne@icloud.com>
2024-03-12 09:51:51 +0100

    change token

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const fs = require('fs-extra');
 const _ = require('lodash');
 
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN || process.argv[2] || 'secret';
+const GITHUB_TOKEN = process.env.FIFI_TOKEN // || process.argv[2] || 'secret';
 const contributors_file = './social-data/contributors.json';
 const filterOut = [
   'project-template',
@@ -22,8 +22,8 @@ function getCommits(repo, attempts) {
   return fetch(`https://api.github.com/repos/MAIF/${repo}/stats/contributors`, {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      authorization: `Bearer ${GITHUB_TOKEN}`
+      Accept: 'application/vnd.github+json',
+      Authorization: `Bearer ${GITHUB_TOKEN}`
     }
   }).then(r => {
     console.log(repo, r.status)
